3a530b0b30b0d5592ac80c712481f63c34107d7c
Audun Sørheim <audun.sorheim@nav.no>
2026-01-20 20:36:10 +0100

    feat: Remove unused lumiApiTransport module and related tests for cleaner codebase

--- packages/lumi-survey/src/core/index.ts
+++ packages/lumi-survey/src/core/index.ts
@@ -8,7 +8,6 @@ export { ErrorType as LumiApiErrorType } from "../contracts/lumiApi";
 
 export * from "./branchingEngine";
 export * from "./evaluateVisibility";
-export * from "./lumiApiTransport";
 export * from "./ratingLabels";
 export * from "./types";
 export * from "./useLumiSurvey";
